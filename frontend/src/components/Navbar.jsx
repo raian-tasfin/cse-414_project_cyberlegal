5d6d2984b720e5c64b91a870cc741564290a75f3
Raian Tasfin <raian.tasfin-prog@yandex.com>
2024-12-14 07:34:34 +0600

    navbar/ioslated UserMenu. Need cleanup.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -17,32 +17,24 @@ import {
 import MenuIcon from '@mui/icons-material/Menu';
 import { useTheme } from '@mui/material/styles';
 import Logo from '@home/components/Logo';
+import UserMenu from '@home/components/UserMenu';
 
 
 
 const pages = ['Research', 'Contribute', 'Connect'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
 
 
 function Navbar() {
     const theme = useTheme();
     const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
 
     const handleOpenNavMenu = (event) => {
         setAnchorElNav(event.currentTarget);
     };
-    const handleOpenUserMenu = (event) => {
-        setAnchorElUser(event.currentTarget);
-    };
 
     const handleCloseNavMenu = () => {
         setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-        setAnchorElUser(null);
     };
 
     return (
@@ -53,10 +45,14 @@ function Navbar() {
         >
             <Container maxWidth="xl">
                 <Toolbar disableGutters>
+
+                    {/* Full Screen Logo */}
                     <Box sx={{ flexGrow: 0.01 }} />
                     <Logo />
                     <Box sx={{ flexGrow: 0.01 }} />
 
+
+                    {/* Small Screen Menu Icon */}
                     <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
                         <IconButton
                             size="large"
@@ -91,12 +87,16 @@ function Navbar() {
                             ))}
                         </Menu>
                     </Box>
+
+                    {/* Small Screen Logo */}
                     <Logo
                         sx={{
                             display: { xs: 'flex', md: 'none' },
                             flexGrow: 1,
                         }}
                     />
+
+                    {/* Full Screen Nav Options */}
                     <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                         {pages.map((page) => (
                             <Button
@@ -108,35 +108,10 @@ function Navbar() {
                             </Button>
                         ))}
                     </Box>
-                    <Box sx={{ flexGrow: 0 }}>
-                        <Tooltip title="Open settings">
-                            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                            </IconButton>
-                        </Tooltip>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            id="menu-appbar"
-                            anchorEl={anchorElUser}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'right',
-                            }}
-                            open={Boolean(anchorElUser)}
-                            onClose={handleCloseUserMenu}
-                        >
-                            {settings.map((setting) => (
-                                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                                    <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
+
+
+                    {/* Account */}
+                    <UserMenu sx={{ flexGrow: 0 }} />
                 </Toolbar>
             </Container>
         </AppBar>
